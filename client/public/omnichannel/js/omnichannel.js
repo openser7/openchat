9d95315e1435f5e1cfcefd6c97ac8b476e12a245
Openser <openser.prueba@gmail.com>
2020-05-15 11:18:21 +0000

    modificacion para widget

--- client/public/omnichannel/js/omnichannel.js
+++ client/public/omnichannel/js/omnichannel.js
@@ -1,39 +1,3 @@
-localStorage.source = "widget";
-JSON.tryParse = function(str){
-    try {
-        return JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-}
-
-String.prototype.matchAny = function(array) {
-    var match = false,
-        str = this;
-    if (typeof array != "object") return false;
-    array.forEach(function(item) {
-        if (str == item) {
-            match = true;
-            return;
-        }
-    });
-    return match;
-}
-
-function uuidv4() {
-	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	  var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-	  return v.toString(16);
-	});
-  }
-
-  
-  function getDateStringToDate(str){
-	
-	return {
-		dateObj : new Date()
-	}
-}
 
 
 $OMNICHANNEL = {
--- client/public/omnichannel/widget.js
+++ client/public/omnichannel/widget.js
@@ -9,6 +9,48 @@ var _IdUsuario = 1 ;//Representa usuario anonimo
 var _source="widget";
 _source ="openser";
 
+JSON.tryParse = function(str){
+    try {
+        return JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+}
+
+String.prototype.matchAny = function(array) {
+    var match = false,
+        str = this;
+    if (typeof array != "object") return false;
+    array.forEach(function(item) {
+        if (str == item) {
+            match = true;
+            return;
+        }
+    });
+    return match;
+}
+
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	  var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+	  return v.toString(16);
+	});
+  }
+
+  
+  function getDateStringToDate(str){
+	
+	return {
+		dateObj : new Date()
+	}
+}
+
+$confirm = function(title,msg,fn){
+    var x = confirm(title+ "  "+ msg);
+    if(x)fn("yes");
+
+}
+
 function ErrorEnConexion(){
     console.log("Error de conexion, recarga el chat");
 }
